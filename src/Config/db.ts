66fd5cd64bcbef3b9ca4a9e85f3942056c25662d
sakthi-ionixx <sakthivelrajan.b@ionixxtech.com>
2025-09-17 14:34:42 +0530

    Add MongoDB credential from env

--- src/Config/db.ts
+++ src/Config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDb = async () => {
-    const encodedUsername = encodeURIComponent('sakthivelrajan');
-    const encodedPassword = encodeURIComponent('Qcsf5hpwaX9UtcaP');
-    await mongoose.connect(`mongodb+srv://${encodedUsername}:${encodedPassword}@movies.1iyj5db.mongodb.net/?retryWrites=true&w=majority&appName=Movies`).then(() => { console.log('Database conected'); }).catch((err: unknown) => { console.log(err, 'error'); })
+    const encodedUsername = encodeURIComponent(process.env.MONGO_USER_NAME ?? '');
+    const encodedPassword = encodeURIComponent(process.env.MONGO_USER_PASSWORD ?? '');
+    await mongoose.connect(`mongodb+srv://${encodedUsername}:${encodedPassword}@${process.env.MONGO_COLLECTION ?? ''}/?retryWrites=true&w=majority&appName=Movies`).then(() => { console.log('Database conected'); }).catch((err: unknown) => { console.log(err, 'error'); })
 }
